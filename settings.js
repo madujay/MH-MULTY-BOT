525df2b2325c35d135b3f997ad4ed3309b06e22b
madujay <113659360+madujay@users.noreply.github.com>
2022-09-23 19:51:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,9 +43,9 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711613510']
-global.ownertag = '94711613510' //ur tag number
-global.botnumber = '94711572553' //Add your Bot Number For Log
+global.owner = ['94721613510']
+global.ownertag = '94721613510' //ur tag number
+global.botnumber = '94721613510' //Add your Bot Number For Log
 global.botname = 'CYBER-X BETA EDITION' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
