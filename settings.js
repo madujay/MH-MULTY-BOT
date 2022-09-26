9e8876e7560b3cd4ab486d29e4ba3850f4e58a15
madujay <113659360+madujay@users.noreply.github.com>
2022-09-26 07:48:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -44,7 +44,7 @@ global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
 global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
+global.ownertag = '94711572553' //ur tag number
 global.botnumber = '94764679736' //Add your Bot Number For Log
 global.botname = 'CYBER-X BETA EDITION' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
